a5adc409f0ddbf42837daaf60e6aa674f55594dd
julio hernandez <hernandezjulio58@ymail.com>
2019-11-30 10:12:04 -0800

    working on auth

--- src/validate-bearer-token.js
+++ src/validate-bearer-token.js
@@ -4,9 +4,9 @@ const logger = require('./logger');
 function validateBearerToken(req, res, next) {
   const apiToken = API_TOKEN;
   const authToken = req.get('Authorization');
-  console.log(authToken.split(' ')[1]);
+  console.log(authToken.split(' ')[1], apiToken);
 
-  if (!authToken || authToken.split(' ')[1] !== API_TOKEN) {
+  if (!authToken || authToken.split(' ')[1] !== apiToken) {
     logger.error(`Unauthorized request to path: ${req.path}`);
     return res.status(401).json({ error: 'Unauthorized request' });
   }
