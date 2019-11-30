2df637295483ceeb7de6e1ac1c5b7b2b167a30b6
julio hernandez <hernandezjulio58@ymail.com>
2019-11-30 10:08:37 -0800

    apitoken issue

--- src/validate-bearer-token.js
+++ src/validate-bearer-token.js
@@ -6,7 +6,7 @@ function validateBearerToken(req, res, next) {
   const authToken = req.get('Authorization');
   console.log(authToken.split(' ')[1]);
 
-  if (!authToken || authToken.split(' ')[1] !== apiToken) {
+  if (!authToken || authToken.split(' ')[1] !== API_TOKEN) {
     logger.error(`Unauthorized request to path: ${req.path}`);
     return res.status(401).json({ error: 'Unauthorized request' });
   }
